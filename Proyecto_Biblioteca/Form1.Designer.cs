93feb21896026d0ecaff2623c59a169436428777
miguel <garcia9903.201.angel@gmail.com>
2021-11-07 22:09:11 -0600

    ESTA ES LA NUEVA CAPERTA SOBRE EL TRBAJO

---
+++ Proyecto_Biblioteca/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace Proyecto_Biblioteca
+{
+    partial class frameUsuario
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frameUsuario));
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPaterno = new System.Windows.Forms.TextBox();
+            this.textMaterno = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Fecha_Nac = new System.Windows.Forms.DateTimePicker();
+            this.ComboGenero = new System.Windows.Forms.ComboBox();
+            this.txtDomicilio = new System.Windows.Forms.TextBox();
+            this.txtPoblacion = new System.Windows.Forms.TextBox();
+            this.txtProvidencia = new System.Windows.Forms.TextBox();
+            this.btmRegistrae = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.BbtnEliminar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.txtProvidencia);
+            this.groupBox1.Controls.Add(this.txtPoblacion);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtDomicilio);
+            this.groupBox1.Controls.Add(this.ComboGenero);
+            this.groupBox1.Controls.Add(this.Fecha_Nac);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.textMaterno);
+            this.groupBox1.Controls.Add(this.txtPaterno);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtMatricula);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // txtMatricula
+            // 
+            resources.ApplyResources(this.txtMatricula, "txtMatricula");
+            this.txtMatricula.Name = "txtMatricula";
+            // 
+            // txtNombre
+            // 
+            resources.ApplyResources(this.txtNombre, "txtNombre");
+            this.txtNombre.Name = "txtNombre";
+            // 
+            // txtPaterno
+            // 
+            resources.ApplyResources(this.txtPaterno, "txtPaterno");
+            this.txtPaterno.Name = "txtPaterno";
+            this.txtPaterno.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textMaterno
+            // 
+            resources.ApplyResources(this.textMaterno, "textMaterno");
+            this.textMaterno.Name = "textMaterno";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Fecha_Nac
+            // 
+            this.Fecha_Nac.AllowDrop = true;
+            resources.ApplyResources(this.Fecha_Nac, "Fecha_Nac");
+            this.Fecha_Nac.CalendarForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Fecha_Nac.Name = "Fecha_Nac";
+            // 
+            // ComboGenero
+            // 
+            this.ComboGenero.FormattingEnabled = true;
+            this.ComboGenero.Items.AddRange(new object[] {
+            resources.GetString("ComboGenero.Items"),
+            resources.GetString("ComboGenero.Items1")});
+            resources.ApplyResources(this.ComboGenero, "ComboGenero");
+            this.ComboGenero.Name = "ComboGenero";
+            // 
+            // txtDomicilio
+            // 
+            resources.ApplyResources(this.txtDomicilio, "txtDomicilio");
+            this.txtDomicilio.Name = "txtDomicilio";
+            // 
+            // txtPoblacion
+            // 
+            resources.ApplyResources(this.txtPoblacion, "txtPoblacion");
+            this.txtPoblacion.Name = "txtPoblacion";
+            // 
+            // txtProvidencia
+            // 
+            resources.ApplyResources(this.txtProvidencia, "txtProvidencia");
+            this.txtProvidencia.Name = "txtProvidencia";
+            // 
+            // btmRegistrae
+            // 
+            this.btmRegistrae.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btmRegistrae.FlatAppearance.BorderSize = 2;
+            resources.ApplyResources(this.btmRegistrae, "btmRegistrae");
+            this.btmRegistrae.Name = "btmRegistrae";
+            this.btmRegistrae.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.btnModificar.FlatAppearance.BorderSize = 2;
+            resources.ApplyResources(this.btnModificar, "btnModificar");
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnEliminar.FlatAppearance.BorderSize = 2;
+            resources.ApplyResources(this.btnEliminar, "btnEliminar");
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // BbtnEliminar
+            // 
+            this.BbtnEliminar.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.BbtnEliminar.FlatAppearance.BorderSize = 2;
+            resources.ApplyResources(this.BbtnEliminar, "BbtnEliminar");
+            this.BbtnEliminar.Name = "BbtnEliminar";
+            this.BbtnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.btmRegistrae);
+            this.panel1.Controls.Add(this.BbtnEliminar);
+            this.panel1.Controls.Add(this.btnModificar);
+            this.panel1.Controls.Add(this.btnEliminar);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // frameUsuario
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Gray;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frameUsuario";
+            this.Opacity = 0.8D;
+            this.TransparencyKey = System.Drawing.Color.Silver;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textMaterno;
+        private System.Windows.Forms.TextBox txtPaterno;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DateTimePicker Fecha_Nac;
+        private System.Windows.Forms.ComboBox ComboGenero;
+        private System.Windows.Forms.TextBox txtDomicilio;
+        private System.Windows.Forms.TextBox txtProvidencia;
+        private System.Windows.Forms.TextBox txtPoblacion;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btmRegistrae;
+        private System.Windows.Forms.Button BbtnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ Proyecto_Biblioteca/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Biblioteca
+{
+    public partial class frameUsuario : Form
+    {
+        public frameUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
