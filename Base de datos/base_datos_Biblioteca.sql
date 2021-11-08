fc716e9f286c876caabd0c336ef03907e9f80efe
miguel <garcia9903.201.angel@gmail.com>
2021-11-07 22:03:19 -0600

    CARPETA INVOLUCRA CON BASE DE DATOS

--- Base de datos/base_datos_Biblioteca.sql
+++ Base de datos/base_datos_Biblioteca.sql
@@ -4,15 +4,12 @@ Miguel Angel Florencio Isabel (Patron)
 Miguel Angel Garcia Martinez  (Apenas va)
 Mateteria: Paginas Web
 Hacer una pagina Web de forma de manera Local*/
-
 -- Creando la Base de Datos
 CREATE DATABASE bd_Biblioteca;
 -- Usando la Base De datos
 Use bd_Biblioteca;
-
 -- Creando la tabla "Usuario"
 CREATE TABLE IF NOT EXISTS Usuario (
-  id_Isuario INT auto_increment,
   Matricula INTEGER UNIQUE ,
   Nombre VARCHAR(35) NOT NULL,
   Apellido_Paterno VARCHAR(35) NOT NULL,
@@ -22,12 +19,13 @@ CREATE TABLE IF NOT EXISTS Usuario (
   Domicilio VARCHAR(70) NULL DEFAULT 'S/E',
   Poblacion VARCHAR(35) NOT NULL,
   Providencia VARCHAR(60) NOT NULL,
-  CONSTRAINT Pk_Usuario PRIMARY KEY (id_Isuario))
+  CONSTRAINT Pk_Usuario PRIMARY KEY (Matricula))
   ENGINE = InnoDB;
+  drop table Usuario;
 
 -- Creando la base de datos "Libros"
 CREATE TABLE IF NOT EXISTS Libro (
-  Id_Libro INT NOT NULL auto_increment,
+  Id_Libro VARCHAR(12) NOT NULL,
   Nombre VARCHAR(70) NOT NULL,
   Editorial VARCHAR(70) NOT NULL,
   Autor VARCHAR(60) NOT NULL,
@@ -35,15 +33,16 @@ CREATE TABLE IF NOT EXISTS Libro (
   Pais_Autor VARCHAR(30) NOT NULL,
   Numero_Pagina INT NOT NULL,
   Year_Edicion DATE NOT NULL,
-  Precio_Libro DOUBLE default 0,
+  Precio_Libro  DECIMAL(5,3) default 0,
   Descripcion VARCHAR(200) NULL,
   CONSTRAINT Pk_Libro1 PRIMARY KEY (Id_Libro))
 ENGINE = InnoDB;
+ALTER TABLE Libro MODIFY COLUMN Precio_Libro decimal(5,3) default 0;
 
 -- Creacion de la tabla Registro
 CREATE TABLE IF NOT EXISTS Registro (
   Id_Registro INT NOT NULL auto_increment,
-  Usuario_id_Isuario INT NOT NULL,
+  Usuario_id_Isuario INTEGER NOT NULL,
   Libro_Id_Libro INT NOT NULL,
   Fecha_Salida DATE NOT NULL,
   Fecha_Maxima DATE NOT NULL,
@@ -56,6 +55,6 @@ ENGINE = InnoDB;
 -- Mostrando lista de la tabla de la base de datos 
   SHOW TABLES FROM biblio;
   -- Mostrando el contenido de la tabla usuario
-  show columns from Usuario;
+  show columns from Libro;
 -- Liga ayuda
 -- https://www.jveweb.net/archivo/2011/03/manejando-mysql-desde-la-linea-de-comandos.html#jveweb_es_029_07
